90d25da9bb22985af649ea504c11018ab69b1c4f
Ovederax <alekseyblagoy@gmail.com>
2020-07-09 12:30:05 +0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'wiki-table';
+  wiki = [
+    {
+      pageid: 18846922,
+      title: 'Craig Noone',
+      snippet: '<span class="searchmatch">Craig</span> Stephen <span class="searchmatch">Noone</span> (born 17 November 1987) is an English professional footballer who plays as a winger for Melbourne City. Born in Kirkby, he has also',
+      timestamp: '2020-06-20T09:49:26Z'
+    }
+  ];
 }
